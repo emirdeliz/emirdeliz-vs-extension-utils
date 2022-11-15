90b026dcee42ed3768a25910c46debe88bf2fadc
emirdeliz <emir.marques@condoconta.com>
2022-11-15 19:14:03 -0300

    feat: Work in progress

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,9 @@
 import path from 'path';
+import Os from 'os';
+
+function isLinux() {
+	return Os.platform() === 'linux';
+}
 
 export const EMIRDELIZ_EXTENSION_UTILS_TERMINAL_PREFIX_NAME = 'Ext utils';
 export const EMIRDELIZ_EXTENSION_UTILS_VSCODE_SETTINGS_PREFIX = 'emirdeliz-';
@@ -8,11 +13,8 @@ export const EMIRDELIZ_EXTENSION_UTILS_GIT_COMMANDS = {
 	merge: 'merge,',
 };
 
-const currentDir = path.resolve(process.cwd(), __dirname);
-
-console.debug({
-	currentDir,
-});
-
+const currentDir = path.resolve(__dirname, `../${isLinux() ? '../' : ''}src`);
 export const EMIRDELIZ_TEST_WORKSPACE_PATH = `${currentDir}/test/__mocks__/folders`;
 export const EMIRDELIZ_TEST_WORKSPACE_FOLDER_FOCUS = `${currentDir}/utils`;
+
+console.log({ currentDir });
